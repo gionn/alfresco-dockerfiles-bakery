d7b7afece041c071240be7e08651c7531743d43b
Paweł Maciusiak <158472457+pmacius@users.noreply.github.com>
2024-07-17 17:36:39 +0200

    OPSEXP-2732 Add search enterprise metadata image Dockerfile (#1)

--- scripts/fetch-artifact.sh
+++ scripts/fetch-artifact.sh
@@ -7,8 +7,14 @@ DEFAULT_ARTIFACT_NAME=alfresco-content-services-distribution
 ARTIFACT_VERSION=$2
 DEFAULT_ARTIFACT_VERSION=23.2.1
 
-GROUP_ID=$3
+EXTENSION=$3
+DEFAULT_EXTENSION=.zip
+
+GROUP_ID=$4
 DEFAULT_GROUP_ID=org.alfresco
+
+PATH_FOR_ARTIFACT=$5
+DEFAULT_PATH_FOR_ARTIFACT=./
 
 if [ -z "$ARTIFACT_NAME" ]; then
     ARTIFACT_NAME=$DEFAULT_ARTIFACT_NAME
@@ -18,13 +24,21 @@ if [ -z "$ARTIFACT_VERSION" ]; then
     ARTIFACT_VERSION=$DEFAULT_ARTIFACT_VERSION
 fi
 
+if [ -z "$EXTENSION" ]; then
+    EXTENSION=$DEFAULT_EXTENSION
+fi
+
 if [ -z "$GROUP_ID" ]; then
     GROUP_ID=$DEFAULT_GROUP_ID
+fi
+
+if [ -z "$PATH_FOR_ARTIFACT" ]; then
+    PATH_FOR_ARTIFACT=$DEFAULT_PATH_FOR_ARTIFACT
 fi
 
 GROUP_ID_AS_PATH=$(echo "$GROUP_ID" | tr . /)
 
 
 echo "Downloading $GROUP_ID:$ARTIFACT_NAME $ARTIFACT_VERSION from Nexus"
-wget --user "$NEXUS_USERNAME:$NEXUS_PASSWORD" \
-    "https://nexus.alfresco.com/nexus/service/local/repositories/enterprise-releases/content/$GROUP_ID_AS_PATH/$ARTIFACT_NAME/${ARTIFACT_VERSION}/$ARTIFACT_NAME-${ARTIFACT_VERSION}.zip"
+wget "https://nexus.alfresco.com/nexus/service/local/repositories/enterprise-releases/content/$GROUP_ID_AS_PATH/$ARTIFACT_NAME/${ARTIFACT_VERSION}/$ARTIFACT_NAME-${ARTIFACT_VERSION}${EXTENSION}" \
+    -O $PATH_FOR_ARTIFACT/$ARTIFACT_NAME-${ARTIFACT_VERSION}${EXTENSION}
---
+++ search/enterprise/README.md
@@ -0,0 +1,18 @@
+# Runtime variables
+
+Sets of variables configurable with your docker image
+
+## metadata
+
+```yaml
+
+live-indexing-metadata:
+    image: alfresco-enterprise-search-metadata:YOUR-TAG
+    environment:
+        SPRING_ELASTICSEARCH_REST_URIS: http://elasticsearch:9200
+        SPRING_ACTIVEMQ_BROKERURL: nio://activemq:61616
+
+```
+
+- `SPRING_ELASTICSEARCH_REST_URIS` - Elasticsearch server, by default `http://elasticsearch:9200`
+- `SPRING_ACTIVEMQ_BROKERURL` - Alfresco ActiveMQ, by default `nio://activemq:61616`
