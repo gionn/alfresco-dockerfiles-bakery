e2979b91e8867df147e2df42cc523346dc167232
Paweł Maciusiak <158472457+pmacius@users.noreply.github.com>
2024-09-13 15:38:13 +0200

    OPSEXP-2818 Add arm64 build and test (#44)

--- tengine/aio/entrypoint.sh
+++ tengine/aio/entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# Check if the architecture is ARM64 (aarch64)
+if [[ "$(uname -m)" != "x86_64" ]]; then
+  export LIBREOFFICE_HOME=${LIBREOFFICE_HOME:=/usr/lib64/libreoffice}
+fi
+
 exec java $JAVA_OPTS $JAVA_OPTS_CONTAINER_FLAGS -jar /opt/app.jar
