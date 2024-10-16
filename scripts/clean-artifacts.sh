c1fd34378cb19cd181a6d0ff3f36a5c4db88204e
Giovanni Toraldo <71768+gionn@users.noreply.github.com>
2024-10-16 14:14:31 +0200

    OPSEXP-2844 Add script to clean artifacts, fix fetch on error and common cache (#74)

---
+++ scripts/clean-artifacts.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -e
+# Clean all artifacts fetched by fetch-artifacts.sh
+
+REPO_ROOT="$(dirname $0)/.."
+
+FORCE=false
+while getopts "f" opt; do
+  case $opt in
+  f)
+    FORCE=true
+    ;;
+  *) ;;
+  esac
+done
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  FIND_OPTS="-regextype egrep"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  FIND_OPTS="-E"
+else
+  echo "Unsupported OS for find command."
+  exit 1
+fi
+
+files=$(
+  find "$REPO_ROOT" $FIND_OPTS \
+    ! -path '*/artifacts_cache/*' \
+    -regex ".*-.*\.(jar|zip|amp|tgz|gz|rpm|deb)"
+)
+
+if [ -z "$files" ]; then
+  echo "No artifacts found to clean."
+  exit 0
+fi
+
+echo "The following files will be deleted:"
+echo "$files"
+
+if [ "$FORCE" = 'false' ]; then
+  read -p "Do you want to delete these files? (y/n) " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Operation cancelled."
+    exit 0
+  fi
+fi
+
+echo "$files" | xargs rm
+echo "All artifacts deleted."
--- scripts/fetch-artifacts.sh
+++ scripts/fetch-artifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # Fetch artifact from Nexus which will be used to build the Docker image
 
-REPO_ROOT=$(dirname $0)
+REPO_ROOT="$(dirname $0)/.."
 
 ACS_VERSION=${ACS_VERSION:=23.2.2}
 INDEX_KEY=${ACS_VERSION%%.*}
@@ -15,19 +15,31 @@ do_fetch_mvn() {
     ARTIFACT_GROUP=$(jq -r ".artifacts.acs${INDEX_KEY}[$i].group" $1)
     ARTIFACT_PATH=$(jq -r ".artifacts.acs${INDEX_KEY}[$i].path" $1)
     ARTIFACT_BASEURL="https://nexus.alfresco.com/nexus/repository/${ARTIFACT_REPO}"
+    ARTIFACT_CACHE_PATH="${REPO_ROOT}/artifacts_cache/${ARTIFACT_NAME}-${ARTIFACT_VERSION}${ARTIFACT_EXT}"
     ARTIFACT_FINAL_PATH="${ARTIFACT_PATH}/${ARTIFACT_NAME}-${ARTIFACT_VERSION}${ARTIFACT_EXT}"
+    echo # newline for better readability
     if [ -f "${ARTIFACT_FINAL_PATH}" ]; then
       echo "Artifact $ARTIFACT_NAME-$ARTIFACT_VERSION already downloaded, skipping..."
       continue
     fi
+    if [ -f "${ARTIFACT_CACHE_PATH}" ]; then
+      echo "Artifact $ARTIFACT_NAME-$ARTIFACT_VERSION already downloaded in cache, copying..."
+      cp "${ARTIFACT_CACHE_PATH}" "${ARTIFACT_FINAL_PATH}"
+      continue
+    fi
     echo "Downloading $ARTIFACT_GROUP:$ARTIFACT_NAME $ARTIFACT_VERSION from $ARTIFACT_BASEURL"
-    wget "${ARTIFACT_BASEURL}/${ARTIFACT_GROUP//\./\/}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/${ARTIFACT_NAME}-${ARTIFACT_VERSION}${ARTIFACT_EXT}" \
-      -O "${ARTIFACT_FINAL_PATH}" \
-      --no-verbose
+    if ! wget "${ARTIFACT_BASEURL}/${ARTIFACT_GROUP//\./\/}/${ARTIFACT_NAME}/${ARTIFACT_VERSION}/${ARTIFACT_NAME}-${ARTIFACT_VERSION}${ARTIFACT_EXT}" \
+      -O "${ARTIFACT_CACHE_PATH}" \
+      --no-verbose; then
+      rm -f "${ARTIFACT_CACHE_PATH}" # wget leaves a 0 byte file if it fails
+      echo "Skipping after wget failure..."
+    else
+      cp "${ARTIFACT_CACHE_PATH}" "${ARTIFACT_FINAL_PATH}"
+    fi
   done
 }
 
-TARGETS=$(find "${REPO_ROOT}/.." -regex "${REPO_ROOT}/../${1:+$1/}.*" -name artifacts.json -mindepth 2 -print)
+TARGETS=$(find "${REPO_ROOT}" -regex "${REPO_ROOT}/${1:+$1/}.*" -name artifacts.json -mindepth 2 -print)
 
 for i in $TARGETS ; do
   do_fetch_mvn $i
